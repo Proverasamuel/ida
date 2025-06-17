bb79700813a26026ec5853490561d3de7b602348
proverasamuel-sys <provera.samuel@multitel.co.ao>
2025-06-17 09:46:46 +0100

    feat, eevntos

--- eventos-agro/scripts.js
+++ eventos-agro/scripts.js
@@ -2,3 +2,8 @@ document.getElementById("formInscricao").addEventListener("submit", function(e) 
   e.preventDefault();
   alert("Inscrição recebida! Entraremos em contacto em breve.");
 });
+// Adicione isso em script.js
+window.addEventListener('scroll', function() {
+  const nav = document.querySelector('.navbar');
+  nav.classList.toggle('scrolled', window.scrollY > 50);
+});
